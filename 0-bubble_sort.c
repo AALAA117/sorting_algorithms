21f6286ae479a0e62c30d6fd2e050a6e7afb5030
AALAA <alaamglal@gmail.com>
2024-02-20 12:37:38 -0800

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,22 +11,29 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	size_t j;
 	int temp;
+	int swaped;
 
 	if (size < 2)
 	{
 		return;
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		swaped = 0;
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j + 1] < array[j])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				print_array(array, size);
 			}
+			swaped = 1;
 		}
-		print_array(array, size);
+		if (swaped == 0)
+		{
+			break;
+		}
 	}
 }
